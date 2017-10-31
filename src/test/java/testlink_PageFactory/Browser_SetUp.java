08addcb4aec93d435bb7bcc1260a51b256a43b54
Hashantha119 <hashantha@intervest.lk>
2017-10-31 11:42:26 +0530

    seleniumWithTestLink

---
+++ src/test/java/testlink_PageFactory/Browser_SetUp.java
@@ -0,0 +1,37 @@
+package testlink_PageFactory;
+
+/**
+ * Created by Hashantha on 10/18/2017.
+ */
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.PageFactory;
+
+
+public class Browser_SetUp {
+
+    public static String DEV_KEY = "c1b17bbdd838186541b0c7b2efd4c0ee";
+    public static String SERVER_URL = "http://localhost/testlink/testlink/lib/api/xmlrpc/v1/xmlrpc.php";
+    public static String PROJECT_NAME = "flipMyroom";
+    public static String PLAN_NAME = "Phase04";
+    public static String BUILD_NAME = "Reg&Login_Build";
+    public WebDriver driver;
+    public String site = "https://www.flip.net/";
+
+    public void setUp() throws Exception {
+
+        driver = new FirefoxDriver();
+    }
+
+
+    public void sleep()  {
+        try {
+            Thread.sleep(3000);
+        }catch (InterruptedException e){
+            e.getMessage();
+        }
+    }
+
+
+}
