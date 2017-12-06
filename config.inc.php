a201fc688892487ed8efd1ce5b15467612fbc4e2
Hashantha119 <hashantha@intervest.lk>
2017-12-06 16:00:27 +0530

    emailConfig

--- config.inc.php
+++ config.inc.php
@@ -287,12 +287,12 @@ $tlCfg->loggerFilter = null; // default defined on logger.class.php ;
  * Configure using custom_config.inc.php
  * @uses lib/functions/email_api.php
  */
-$g_smtp_host        = '[smtp_host_not_configured]';  # SMTP server MUST BE configured
+$g_smtp_host        = 'smtp.gmail.com';  # SMTP server MUST BE configured
 
 # Configure using custom_config.inc.php
-$g_tl_admin_email     = '[testlink_sysadmin_email_not_configured]'; # for problem/error notification
-$g_from_email         = '[from_email_not_configured]';  # email sender
-$g_return_path_email  = '[return_path_email_not_configured]';
+$g_tl_admin_email     = 'uguest.intervest@gmail.com'; # for problem/error notification
+$g_from_email         = 'uguest.intervest@gmail.com';  # email sender
+$g_return_path_email  = 'uguest.intervest@gmail.com';
 
 /**
  * Email notification priority (low by default)
@@ -318,14 +318,14 @@ $g_smtp_password    = '';  # password
  * Can be '', 'ssl','tls'
  * @global string $g_smtp_connection_mode
  */
-$g_smtp_connection_mode = '';
+$g_smtp_connection_mode = 'ssl';
 
 /**
  * The smtp port to use.  The typical SMTP ports are 25 and 587.  The port to use
  * will depend on the SMTP server configuration and hence others may be used.
  * @global int $g_smtp_port
  */
-$g_smtp_port = 25;                        
+$g_smtp_port = 465;                        
 
 
 /**
